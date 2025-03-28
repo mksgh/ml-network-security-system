f36bb172e72153b8e6a23256e170b3bef94a1ecf
mksgh <mksgh@vestas.com>
2025-03-28 00:46:01 +0000

    updated logger

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,21 +1,22 @@
 import sys
+from networksecurity.logging import logger
 
 class NetworkSecurityException(Exception):
-    def __init__(self, error_message, error_details: sys):
+    def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
-        exc_be, _, exc_tb = error_details.exc_info()
-
-        self.base_exception = exc_be
-        self.lineno = exc_tb.tb_lineno
-        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        _,_,exc_tb = error_details.exc_info()
         
-
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+    
     def __str__(self):
-        return f"{self.base_exception} {self.e}: Error occured in script name [{self.file_name}] at line no. [{self.lineno}] error message [{str(self.error_message)}]"
-
-
-# if __name__ == "__main__":
-#     try:
-#         logger.logging.INFO("test")
-#     except Exception as e:
-#         raise NetworkSecurityException(e, sys)
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        self.file_name, self.lineno, str(self.error_message))
+        
+if __name__=='__main__':
+    try:
+        logger.logging.info("Enter the try block")
+        a=1/0
+        print("This will not be printed",a)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
